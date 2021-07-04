2a75d6617f4e48b22d5e617556f3d54685774dc8
gaberani <khs0783@naver.com>
2021-07-04 23:21:45 +0900

    Add: todo odd list with react

--- 2장/1-element/App.js
+++ 2장/1-element/App.js
@@ -4,6 +4,7 @@ export default function App() {
   const [todoList, setTodoList] = useState([]);
   const [currentId, setCurrentId] = useState(1);
   const [desc, setDesc] = useState('');
+  const [showOdd, setShowOdd] = useState(false);
   function onAdd() {
     const todo = { id: currentId, desc };
     setCurrentId(currentId + 1);
@@ -19,7 +20,9 @@ export default function App() {
     <div>
       <h3>할 일 목록</h3>
       <ul>
-        {todoList.map(todo => (
+        {todoList
+          .filter((_, index) => ( showOdd ? index % 2 === 0 : true  ))
+          .map(todo => (
           <li key={todo.id}>
             <span>{todo.desc}</span>
             <button data-id={todo.id} onClick={onClick}>
@@ -30,6 +33,9 @@ export default function App() {
       </ul>
       <input type="text" value={''} onChange={e => {}} />
       <button onClick={onAdd}>추가</button>
+      <button onClick={setShowOdd(!showOdd)}>
+        홀수 아이템만 보기 on/off
+      </button>
       <button onClick={onSaveToServer}>서버에 저장</button>
     </div>
   )
