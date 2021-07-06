299c5e900ebc1add22400e6a4f301223108a6f5f
gaberani <khs0783@naver.com>
2021-07-06 23:50:53 +0900

    Test: props value is immutable

--- 2장/state/Title.js
+++ 2장/state/Title.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export default function Tilte({ title }) {
+// function Tilte(props) {
+//   props.title = 'asdf';
+// }
+// read only 인 props 값을 바꿀 수 없다! => 에러 발생
+function Tilte({ title }) {
   console.log('Title render')
   return <p>{ title }</p>;
 }
