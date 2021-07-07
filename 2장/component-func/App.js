9844de4d2597b69a840fc1252a836eb967f8a9c9
gaberani <khs0783@naver.com>
2021-07-07 23:33:03 +0900

    Test: what is fragment

--- 2장/component-func/App.js
+++ 2장/component-func/App.js
@@ -2,5 +2,15 @@ import React from 'react';
 import Counter from './Counter';
 
 export default function App() {
-  return [<p key={1}>안녕</p>, <p key={2}>하세요</p>];
+  return (
+    <React.Fragment>
+      <p>안녕</p>
+      <p>하세요</p>
+    </React.Fragment>
+    // <> = 축약형으로 React.Fragment와 같다
+    //   <p>안녕</p>
+    //   <p>하세요</p>
+    // <>
+  );
+  // [<p key={1}>안녕</p>, <p key={2}>하세요</p>];
 }
