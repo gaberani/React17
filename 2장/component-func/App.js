1fe11bb0bb43fe54fecd2d4c6fd3a54ab192dc61
gaberani <khs0783@naver.com>
2021-07-07 23:33:31 +0900

    Test: how react render null, boolean value

--- 2장/component-func/App.js
+++ 2장/component-func/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <React.Fragment>
       <p>안녕</p>
       <p>하세요</p>
+      {/* null이나 boolean 값은 렌더링 되지 않는다! */}
+      {null}
+      {false}
+      {true}
     </React.Fragment>
     // <> = 축약형으로 React.Fragment와 같다
     //   <p>안녕</p>
