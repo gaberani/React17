d1c73bfd58e2e1474c0e4ee7f6da7997be19f485
gaberani <khs0783@naver.com>
2021-07-10 04:44:10 +0900

    Test: rerender only seconds

---
+++ 2장/2-virtual-dom/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+export default function App() {
+  const [seconds, setSeconds] = useState()
+  useEffect(() => {
+    useState(() => {
+      setTimeout(() => {
+        setSeconds(v => v + 1);
+      }, 1000);
+    })
+  });
+  return (
+    <div>
+      <h1>안녕하새요</h1>
+      <h2>지금까지 {seconds}초가 지났습니다.</h2>
+    </div>
+  )
+}
