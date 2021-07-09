f6ebf6deb399cf3c02521b5c0765755109774c6a
gaberani <khs0783@naver.com>
2021-07-10 04:54:05 +0900

    Test: component's key value change reset usestate value

--- 2장/2-virtual-dom/App.js
+++ 2장/2-virtual-dom/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Counter from './Counter';
 
 export default function App() {
   const [seconds, setSeconds] = useState()
@@ -12,7 +13,8 @@ export default function App() {
   return (
     // key값이 이전과 달라질 경우, 리액트는 전혀 다른 요소로 판단해서
     // 그와 관련된 다른 부분을 다시 렌더링함
-    <div key={seconds}>
+    <div>
+      <Counter key={seconds} />
       <h1 style={{ color: seconds % 2 ? 'red' : 'blue' }}>안녕하새요</h1>
       <h2>지금까지 {seconds}초가 지났습니다.</h2>
     </div>
