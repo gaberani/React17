fe7e1d641b9bc302acf511033621a6ed5db9d2f3
gaberani <khs0783@naver.com>
2021-07-26 23:56:17 +0900

    Add: what is useEffect(2)

--- 2장/3-hook/5-1-useEffect-api.js
+++ 2장/3-hook/5-1-useEffect-api.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 export default function Profile({ userId }) {
   const [user, setUser] = useState(null);
+  const [value, setValue] = useState(0);
+
   useEffect(() => {
+    console.log(value);
     getUserApi(userId).then(data => setUser(data));
-  }, [userId]);
+    // 이와 같이 value라는 지역변수를 추가하고 의존성 배열에도 추가해야 하기에
+    // 이 부분에서 버그가 자주 일어나는데 어떻게 관리할 수 있을 지에 대해서는
+    // 추후에 설명!
+  }, [userId, value]);
   return (
     <div>
       {!user && <p>사용자 정보를 가져오는 중...</p>}
