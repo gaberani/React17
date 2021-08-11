c6f12dac4edd4e54c90f3884c72551becb38dc6c
gaberani <khs0783@naver.com>
2021-08-11 23:59:43 +0900

    Test: context provide, consumer test

--- 2장/5-context/2-context.js
+++ 2장/5-context/2-context.js
@@ -1,26 +1,33 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 const UserContext = createContext('unknown');
 
 export default function App() {
+  const [name, setName] = useState('mike');
   return (
     <div>
-      <UserContext.Provider value="mike">
+      <UserContext.Provider value={name}>
         <div>상단 메뉴</div>
         <Profile />
         <div>하단 메뉴</div>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
       </UserContext.Provider>
     </div>
   );
 }
 
-function Profile() {
+const Profile = React.memo(function() {
+  console.log(Profile)
   return (
     <div>
       <Greeting />
     </div>
   );
-}
+});
 
 function Greeting({ username }) {
   <UserContext.Consumer>
