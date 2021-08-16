fc9cb1a3b83a16d76de511f29da8a543929780de
gaberani <khs0783@naver.com>
2021-08-16 23:34:48 +0900

    Add: use usecontext hook

---
+++ 2장/5-context/3-context-hook.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from 'react';
+
+const UserContext = createContext('unknown');
+
+export default function App() {
+  const [name, setName] = useState('mike');
+  return (
+    <div>
+      <UserContext.Provider value={name}>
+        <div>상단 메뉴</div>
+        <Profile />
+        <div>하단 메뉴</div>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+const Profile = React.memo(function() {
+  console.log(Profile)
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+});
+
+function Greeting() {
+  const username = useContext(UserContext);
+  return <p>{`${username}님 안녕하세요`}</p>;
+}
