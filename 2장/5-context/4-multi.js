ce686cc8860ca84d32f1263f9de01bdac4443966
gaberani <khs0783@naver.com>
2021-08-16 23:35:01 +0900

    Add: use multi usecontext hooks

---
+++ 2장/5-context/4-multi.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState } from 'react';
+
+const UserContext = createContext('unknown');
+const ThemeContext = createContext('dark');
+
+export default function App() {
+  const [name, setName] = useState('mike');
+  return (
+    <div>
+      <ThemeContext.Provider>
+        <UserContext.Provider value={name}>
+          <div>상단 메뉴</div>
+          <Profile />
+          <div>하단 메뉴</div> 
+        </UserContext.Provider>
+      </ThemeContext.Provider>
+    </div>
+  );
+}
+
+const Profile = React.memo(function() {
+  console.log(Profile)
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+});
+
+function Greeting() {
+  const theme = useContext(ThemeContext);
+  const username = useContext(UserContext);
+  return (
+    <p
+      style={{ color: theme === 'dark' ? 'gray' : 'green' }}
+    >{`${username}님 안녕하세요`}</p>;
+  )
+}
