dbacd349b8a76b7c9cf961259a1c563da8528f44
gaberani <khs0783@naver.com>
2021-08-20 22:04:36 +0900

    Add: use ref property in basic component

--- 2장/6-ref/2-component.js
+++ 2장/6-ref/2-component.js
@@ -9,16 +9,27 @@ export default function App() {
   return (
     <div>
       <InputAndSave inputRef={inputRef} />
+      <Button ref={buttonRef}>저장</Button>
       <button onClick={() => inputRef.current.focus()}>텍스트로 이동</button>
     </div>
   );
 }
 
-function InputAndSave({ inputRef }) {
+// ref와 다른 이름인 속성으로 내려서 사용할 경우
+function InputAndSave({ inputRef, buttonRef }) {
   return (
     <div>
       <input type="text" ref={inputRef} />
-      <button>저장</button>
+      <button ref="buttonRef">저장</button>
     </div>
   );
 }
+
+// 기초적인 컴포넌트에 ref라는 이름으로 활용하고 싶은 경우
+const Button = React.forwardRef(function ({ onClick }, ref) {
+  return (
+    <button onClick={onClick} ref={ref}>
+      저장
+    </button>
+  );
+})
