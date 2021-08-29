700b7e0ae6bc86d7909a33ca7d6b5a497da04cf2
gaberani <khs0783@naver.com>
2021-08-30 00:30:00 +0900

    Add: useLayoutEffect 훅의 사용법과 단점

--- 2장/7-basic-hooks/6-useLayoutEffect.js
+++ 2장/7-basic-hooks/6-useLayoutEffect.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 
 export default function App() {
   const [width, setWidth] = useState(200);
-  useEffect(() => {
+  const boxRef = useRef();
+  useLayoutEffect(() => {
+    console.log(boxRef.current.getBoundingClientRect().width)
     if (width > 500) {
       setWidth(500);
     }
+    // 연산이 많은 함수 추가하여 테스트
+    let v = 0;
+    for(let i=0; i<1000000000; i++) {
+      v += i * 2 + i;
+    }
   }, [width]);
-  return (
+  return (  
     <div>
       <div style={{ width, height: 100, backgroundColor: 'green' }}>test</div>
       <button
